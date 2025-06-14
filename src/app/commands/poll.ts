d386ccfbf869064375062018d2d66252485fb7e9
twlite <46562212+twlite@users.noreply.github.com>
2025-06-14 16:14:46 +0545

    feat: add slash command based poll

--- src/app/commands/poll.ts
+++ src/app/commands/poll.ts
@@ -1,11 +1,37 @@
-import type { CommandData, MessageCommand } from 'commandkit';
+import type { CommandData, ChatInputCommand, MessageCommand } from 'commandkit';
 import type { AiCommand, AiConfig } from 'commandkit/ai';
-import { PermissionsBitField } from 'discord.js';
+import { ApplicationCommandOptionType, PermissionsBitField } from 'discord.js';
 import { z } from 'zod';
 
 export const command: CommandData = {
   name: 'poll',
   description: 'Create a poll',
+  options: [
+    {
+      name: 'question',
+      description: 'The question of the poll',
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+    {
+      name: 'answers',
+      description: 'The answers of the poll separated by commas',
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+    {
+      name: 'allow_multiselect',
+      description: 'Whether the poll allows multiple selections',
+      type: ApplicationCommandOptionType.Boolean,
+      required: false,
+    },
+    {
+      name: 'duration',
+      description: 'The duration of the poll in hours',
+      type: ApplicationCommandOptionType.Number,
+      required: false,
+    },
+  ],
 };
 
 const pollMediaObject = z
@@ -47,8 +73,25 @@ export const aiConfig = {
     .describe('An object representing a poll to include in the message'),
 } satisfies AiConfig;
 
-export const message: MessageCommand = async (ctx) => {
-  await ctx.message.reply('This command can only be used via AI');
+export const chatInput: ChatInputCommand = async (ctx) => {
+  const question = ctx.options.getString('question', true);
+  const answers = ctx.options.getString('answers', true);
+  const allowMultiselect = ctx.options.getBoolean('allow_multiselect');
+  const duration = ctx.options.getNumber('duration');
+
+  await ctx.interaction.reply({
+    poll: {
+      allowMultiselect: !!allowMultiselect,
+      answers: answers.split(',').map((answer) => {
+        return {
+          text: answer.trim(),
+          emoji: '',
+        };
+      }),
+      duration: duration ?? 24,
+      question: { text: question },
+    },
+  });
 };
 
 export const ai: AiCommand<typeof aiConfig> = async (ctx) => {
