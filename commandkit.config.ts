2e7b1e6466f72b1fa12612ab7137c589f4263cb8
twlite <46562212+twlite@users.noreply.github.com>
2025-07-27 19:36:54 +0545

    feat: reminder command

--- commandkit.config.ts
+++ commandkit.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'commandkit/config';
 import { ai } from '@commandkit/ai';
+import { tasks } from '@commandkit/tasks';
 
 export default defineConfig({
-  plugins: [ai()],
+  plugins: [ai(), tasks()],
 });
